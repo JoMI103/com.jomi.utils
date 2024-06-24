4cec9f74b896e041d783335f95020d8b8fea601c
JoMI103 <a21078@alunos.ipca.pt>
2024-06-24 14:51:08 +0100

    Update 1.0.4
    Adding a menu function to reset pivot

--- Runtime/MenuFunctions.cs
+++ Runtime/MenuFunctions.cs
@@ -11,6 +11,19 @@ namespace jomi.utils
         [MenuItem("JomiHelper/AddSeparator")]
         static void AddSeparator() => new GameObject("---------------------").transform.position = Vector3.zero;
 
+        [MenuItem("JomiHelper/ResetPivot")]
+        static void ResetPivot()
+        {
+            foreach (GameObject g in Selection.gameObjects)
+            {
+                if(g.transform.parent == null) continue;
+                if(!g.TryGetComponent(out MeshFilter mf)) continue;
+                
+                Vector3 finalPos = mf.sharedMesh.bounds.center - new Vector3(0, mf.sharedMesh.bounds.extents.y, 0);
+                g.transform.localPosition = -finalPos;
+                
+            }
+        }
 #endif
 
     }
